54b46788840462c6a31f5e9849d280c39b6e05b4
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-12-01 01:48:47 +0300

    Добавьте файлы проекта.

---
+++ Task17/Gcd.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task17
+{
+    static class Gcd
+    {
+        public delegate int Algorithm(int x, int y);
+        public static int EuclideanAlgorithm(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            return a;
+        }
+        public static int BinaryAlgorithm(int a, int b)
+        {
+            int k = 1;
+            while ((a != 0) && (b != 0))
+            {
+                while ((a % 2 == 0) && (b % 2 == 0))
+                {
+                    a /= 2;
+                    b /= 2;
+                    k *= 2;
+                }
+                while (a % 2 == 0) a /= 2;
+                while (b % 2 == 0) b /= 2;
+                if (a >= b) a -= b; else b -= a;
+            }
+            return b * k;
+        }
+        public static int CalculateGcd(Algorithm a, params int[] arg)
+        {
+            if (arg.Length < 2)
+                return -1;
+
+            int gcd = a(arg[0], arg[1]);
+            for (int i = 2; i < arg.Length; i++)
+                gcd = a(gcd, arg[i]);
+            return gcd;
+        }
+    }
+}
---
+++ Task17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Task17
+{
+    class Program
+    {
+        private static Stopwatch sw = new Stopwatch();
+        public static void FirstThread()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is executing Gcd.CalculateGcd(Gcd.BinaryAlgorithm, 4, 16, 16, 8)");
+            sw.Start();
+            int x = Gcd.CalculateGcd(Gcd.BinaryAlgorithm, 4, 16, 16, 8);
+            sw.Stop();
+            sw.Reset();
+            Console.WriteLine("x = " + x);
+            Console.WriteLine(sw.Elapsed);
+        }
+        public static void SecondThread()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is executing Gcd.CalculateGcd(Gcd.EuclideanAlgorithm, 4, 16, 16, 8)");
+            sw.Start();
+            int y = Gcd.CalculateGcd(Gcd.EuclideanAlgorithm, 4, 16, 16, 8);
+            sw.Stop();
+            sw.Reset();
+            Console.WriteLine("y = " + y);
+            Console.WriteLine(sw.Elapsed);
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(FirstThread);
+            Thread t2 = new Thread(SecondThread);
+            t1.Start();
+            t1.Join();
+            t2.Start();
+            t2.Join();
+        }
+    }
+}
